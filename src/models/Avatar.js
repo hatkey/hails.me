68e797ef547584676d1bf8bd846a660e2cbe30da
Hayley <hayley@hails.me>
2021-01-16 18:01:09 -0500

    Created Avatar model.

---
+++ src/models/Avatar.js
@@ -0,0 +1,18 @@
+// src/models/Avatar.js
+var m = require("mithril");
+
+var Avatar = {
+    src: null,
+    images: [
+        "hayley_1_circle.png",
+        "hayley_2_circle.png",
+        "hayley_3_circle.png"
+    ],
+    path: "resources/images/avatars/",
+    getRandom: function() {
+        let n = Math.floor(Math.random() * this.images.length);
+        this.src = this.path + this.images[n];
+    }
+};
+
+module.exports = Avatar;
