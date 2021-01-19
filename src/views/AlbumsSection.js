06eb4bda83372f2adc226a423f5fc924211efbdd
Hayley <hayley@hails.me>
2021-01-19 14:25:02 -0500

    Created AlbumsSection component.

---
+++ src/views/AlbumsSection.js
@@ -0,0 +1,24 @@
+// src/views/Music.js
+var m = require("mithril");
+var Album = require("./Album");
+var AlbumList = require("./AlbumList");
+var Albums = require("../models/Albums");
+
+module.exports = {
+    view: () => (
+        <section id="albums">
+            <h2>albums & releases</h2>
+
+            <AlbumList>
+                {Albums.albums.map(album => (
+                    <Album 
+                        name={album.name}
+                        imgSrc={album.imgSrc}
+                        tags={album.tags}
+                        link={album.link}
+                    />
+                ))}
+            </AlbumList>
+        </section>
+    )
+};
