e82f61cbc3c2147a0ae7fe22a726cfbbf3ae76c1
Hayley <hayley@hails.me>
2021-01-19 05:37:19 -0500

    Created SkillsSection component.

---
+++ src/views/SkillsSection.js
@@ -0,0 +1,26 @@
+// src/views/SkillsSection.js
+var m = require("mithril");
+var GridList = require("./GridList");
+var Skills = require("../models/Skills");
+
+module.exports = {
+    view: () => (
+        <section id="skills">
+            <h2>skills</h2>
+    
+            <p><small>
+                Proficiency: 
+                <span class="indicator indicator-high"></span>High 
+                <span class="indicator indicator-medium"></span>Medium 
+                <span class="indicator indicator-low"></span>Low
+                <br /><br />
+            </small></p>
+
+            <GridList>
+                {Skills.skills.map(skill => (
+                    <li class={skill.proficiency}>{skill.name}</li>
+                ))}
+            </GridList>
+        </section>
+    )
+};
