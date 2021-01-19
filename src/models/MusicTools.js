ebbedaaf4067d725f486d8ae7684506038cf5a24
Hayley <hayley@hails.me>
2021-01-19 09:45:48 -0500

    Created MusicTools model.

---
+++ src/models/MusicTools.js
@@ -0,0 +1,24 @@
+// src/models/MusicTools.js
+
+var MusicTools = {
+    tools: [
+        "MOTU M2",
+        "Cockos REAPER",
+        "ReaPlugs",
+        "SoundToys Plugins",
+        "Waves Plugins",
+        "Fabfilter Plugins",
+        "Toneboosters Plugins",
+        "iZotope Plugins",
+        "Vladislav Goncharov Limiter No6",
+        "Helm by Matt Tytel",
+        "NI Massive",
+        "NI Kontakt",
+        "Spitfire LABS",
+        "Spitfire Originals",
+        "Magical 8-bit Plug",
+        "XLN Audio Addictive Drums"
+    ]
+};
+
+module.exports = MusicTools;
