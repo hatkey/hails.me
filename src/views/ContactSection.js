60d29f14ba27587b6557fccb553cbd463410f940
Hayley <hayley@hails.me>
2021-01-19 06:47:47 -0500

    Created ContactSection component.

---
+++ src/views/ContactSection.js
@@ -0,0 +1,15 @@
+// src/views/ContactSection
+var m = require("mithril");
+
+module.exports = {
+    view: () => (
+        <section id="contact">
+            <h2>contact</h2>
+    
+            <p>
+                the best way to get in touch is to &#8203;
+                <a href="mailto:hayley@hails.me">email me</a>.
+            </p>
+        </section>
+    )
+};
