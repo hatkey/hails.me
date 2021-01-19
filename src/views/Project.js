efa3f24875da4895d12b8093f79a0a69abbfd8d1
Hayley <hayley@hails.me>
2021-01-19 06:40:25 -0500

    Created Project component.

---
+++ src/views/Project.js
@@ -0,0 +1,33 @@
+// src/views/Project.js
+var m = require("mithril");
+const BadgeList = require("./BadgeList");
+
+module.exports = {
+    view: (vnode) => (
+        <main><br /><br /><br />
+        <section class="project">
+            <h4>{vnode.attrs.title}</h4>
+
+            <BadgeList dark>
+                {
+                    vnode.attrs.tags ? 
+                    vnode.attrs.tags.map(tag => (<li class="small">{tag}</li>)) : null
+                }
+            </BadgeList>
+
+            <p>{vnode.attrs.description}</p>
+
+            <p>
+                {
+                    vnode.attrs.links ? 
+                    vnode.attrs.links.map(
+                        link => (
+                            <a href={link.url}>{link.name}</a>
+                        )
+                    ) : null
+                }
+            </p>
+
+        </section></main>
+    )
+};
