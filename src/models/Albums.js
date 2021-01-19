2def7df2d33539aee0366e1294c77349387def64
Hayley <hayley@hails.me>
2021-01-19 14:24:21 -0500

    Created models for albums and projects.

---
+++ src/models/Albums.js
@@ -0,0 +1,15 @@
+// src/models/Albums.js
+
+var Albums = {
+    imgPath: "resources/images/albums/",
+    albums: [
+        // {
+        //     name: "Test Album",
+        //     imgSrc: this.imgPath + "test.png",
+        //     tags: ["tag1", "tag2", "tag3"],
+        //     link: "#"
+        // }
+    ]
+};
+
+module.exports = Albums;
---
+++ src/models/Projects.js
@@ -0,0 +1,17 @@
+// src/models/Projects.js
+
+var Projects = {
+    projects: [
+        {
+            name: "hails.me",
+            tags: ["Mithril", "Sass"],
+            description: "Personal website of Hayley LaBrie.",
+            links: [
+                {name: "github", url: "https://github.com/hatkey/hails.me"},
+                {name: "demo", url: "https://hails.me"}
+            ]
+        }
+    ]
+};
+
+module.exports = Projects;
