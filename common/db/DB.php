ffab1d2f28b6ffa595ced83b15547fac7d9b4360
Hayley <hayley@hails.me>
2021-02-09 19:23:25 -0500

    Created parent DB connection class.

---
+++ common/db/DB.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace me\hails\common\db;
+
+define("DB_CONFIG_PATH", "../../../../db_config");
+require DB_CONFIG_PATH . "/db_connect.php";
+
+abstract class DB {
+
+    private $conn = NULL;
+
+
+    public function __construct() {
+        $this->conn = connect();
+    }
+
+    public function __destruct() {
+        if ($this->conn) $this->conn->close();
+    }
+
+    public function isConnected() {
+        return $this->conn !== NULL;
+    }
+
+}
+
+?>
