202eaeb8dc8daedb530476083ea8c3fbe9f60ff7
Hayley <hayley@hails.me>
2021-02-11 22:34:17 -0500

    Added method to AvatarDB class to get all avatars.

--- common/db/AvatarDB.php
+++ common/db/AvatarDB.php
@@ -1,9 +1,26 @@
 <?php
 
 namespace me\hails\common\db;
-require "DB.php";
+
+require_once "DB.php";
+require_once "../models/Avatar.php";
+use me\hails\common\models\Avatar;
 
 class AvatarDB extends DB {
+
+    public function getAllAvatars() : array {
+        $query = "SELECT * FROM avatar;";
+        $result = $this->conn->query($query);
+        
+        if (!$result || $result->num_rows < 1) return [];
+
+        $avatars = [];
+        while($row = $result->fetch_assoc()) {
+            $avatars[] = new Avatar($row["src"]);
+        }
+
+        return $avatars;
+    } // getAllAvatars
 
 }
 
