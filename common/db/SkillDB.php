eff83069bf6103d638a43566d6566f818427102f
Hayley <hayley@hails.me>
2021-02-16 20:23:12 -0500

    Added method to SkillDB class to get all skills.

--- common/db/SkillDB.php
+++ common/db/SkillDB.php
@@ -1,9 +1,29 @@
 <?php
 
 namespace me\hails\common\db;
-require "DB.php";
+
+require_once "DB.php";
+require_once "../models/Skill.php";
+use me\hails\common\models\Skill;
 
 class SkillDB extends DB {
+
+    public function getAllSkills() : array {
+        $query = "SELECT * FROM skill ORDER BY proficiency ASC, name DESC;";
+        $result = $this->conn->query($query);
+        
+        if (!$result || $result->num_rows < 1) return [];
+
+        $skills = [];
+        while($row = $result->fetch_assoc()) {
+            $skills[] = new Skill(
+                $row["name"],
+                Skill::Proficiency[$row["proficiency"]]
+            );
+        }
+
+        return $skills;
+    } // getAllSkills
 
 }
 
