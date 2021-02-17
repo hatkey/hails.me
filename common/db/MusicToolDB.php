82627b4af0692839175f76e6eb81573d1a8a3349
Hayley <hayley@hails.me>
2021-02-16 20:11:10 -0500

    Added method to MusicToolDB class to get all tools

--- common/db/MusicToolDB.php
+++ common/db/MusicToolDB.php
@@ -1,9 +1,26 @@
 <?php
 
 namespace me\hails\common\db;
-require "DB.php";
+
+require_once "DB.php";
+require_once "../models/MusicTool.php";
+use me\hails\common\models\MusicTool;
 
 class MusicToolDB extends DB {
+
+    public function getAllMusicTools() : array {
+        $query = "SELECT * FROM musicTool;";
+        $result = $this->conn->query($query);
+        
+        if (!$result || $result->num_rows < 1) return [];
+
+        $tools = [];
+        while($row = $result->fetch_assoc()) {
+            $tools[] = new MusicTool($row["name"]);
+        }
+
+        return $tools;
+    } // getAllMusicTools
 
 }
 
