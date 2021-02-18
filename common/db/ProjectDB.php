a758a3a0594a6f68b35b971b3bbfa7a92f1607a5
Hayley <hayley@hails.me>
2021-02-17 19:05:31 -0500

    Added methods to get all projects and links/tags.

--- common/db/ProjectDB.php
+++ common/db/ProjectDB.php
@@ -1,9 +1,65 @@
 <?php
 
 namespace me\hails\common\db;
-require "DB.php";
+
+require_once "DB.php";
+require_once "../models/Project.php";
+use me\hails\common\models\Project;
+require_once "../models/ProjectLink.php";
+use me\hails\common\models\ProjectLink;
 
 class ProjectDB extends DB {
+
+    public function getAllProjects() : array {
+        $query = "SELECT * FROM project ORDER BY CASE name WHEN 'hails.me' THEN 0 ELSE 1 END, name;";
+        $result = $this->conn->query($query);
+        
+        if (!$result || $result->num_rows < 1) return [];
+
+        $projects = [];
+        while($row = $result->fetch_assoc()) {
+            $tags = self::getTags($row["name"]);
+            $links = self::getLinks($row["name"]);
+            $projects[] = new Project(
+                $row["name"],
+                $row["description"],
+                $tags,
+                $links
+            );
+        }
+
+        return $projects;
+    } // getAllProjects
+
+    private function getTags(string $projectName) : array {
+        $tags = [];
+
+        $query = "SELECT * FROM projectTag WHERE project='$projectName';";
+        $result = $this->conn->query($query);
+        
+        if ($result && $result->num_rows >= 1) {
+            while ($row = $result->fetch_assoc()) {
+                $tags[] = $row["name"];
+            }
+        }
+
+        return $tags;
+    } // getTags
+
+    private function getLinks(string $projectName) : array {
+        $links = [];
+
+        $query = "SELECT * FROM projectLink WHERE project='$projectName';";
+        $result = $this->conn->query($query);
+        
+        if ($result && $result->num_rows >= 1) {
+            while ($row = $result->fetch_assoc()) {
+                $links[] = new ProjectLink($row["name"], $row["url"]);
+            }
+        }
+
+        return $links;
+    } // getLinks
 
 }
 
